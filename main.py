4e86c9b2618c7e84f98230818354561de381868c
Shobhit <imshobhit.sb@gmail.com>
2021-03-10 20:37:37 +0530

    Toolbar functionality and statusbar

--- main.py
+++ main.py
@@ -108,11 +108,105 @@ text_editor.pack(fill=tk.BOTH,expand=True)
 scroll_bar.config(command=text_editor.yview)
 text_editor.config(yscrollcommand=scroll_bar.set)
 
+                                                       #font family and font size functionality
+current_font_family='Arial'
+current_font_size=12
+#current_font_weight='normal'
+def change_font(application):   #we r passing application as an argument just because of bind function
+    global current_font_family
+    current_font_family=font_family.get()
+    text_editor.configure(font=(current_font_family,current_font_size))
+
+def change_size(application):
+    global current_font_size
+    current_font_size=size_var.get()
+    text_editor.configure(font=(current_font_family,current_font_size))
+
+font_box.bind("<<ComboboxSelected>>",change_font)
+font_size.bind("<<ComboboxSelected>>",change_size)
+
+                                                           #buttons functionality
+
+# print(tk.font.Font(font=text_editor['font']).actual()) This command gives an object of deatils of text_editor
+#Bold button functionality
+def change_bold():
+    text_property=tk.font.Font(font=text_editor['font'])
+    if text_property.actual()['weight']=='normal':
+        text_editor.configure(font=(current_font_family,current_font_size,'bold'))
+    if text_property.actual()['weight']=='bold':
+        text_editor.configure(font=(current_font_family,current_font_size,'normal'))
+
+bold_btn.configure(command=change_bold)
+#Itallic button functionality
+def change_slant():
+    text_property=tk.font.Font(font=text_editor['font'])
+    if text_property.actual()['slant']=='roman':
+        text_editor.configure(font=(current_font_family,current_font_size,'italic'))
+    if text_property.actual()['slant']=='italic':
+        text_editor.configure(font=(current_font_family,current_font_size,'roman'))
+
+italic_btn.configure(command=change_slant)
+
+#Underline button functionality
+
+def change_underline():
+    text_property=tk.font.Font(font=text_editor['font'])
+    if text_property.actual()['underline']==0:
+        text_editor.configure(font=(current_font_family,current_font_size,'underline'))
+    if text_property.actual()['underline']==1:
+        text_editor.configure(font=(current_font_family,current_font_size,'normal'))
+
+underline_btn.configure(command=change_underline)
+
+#font colour functionality
+
+def change_font_color():
+    color_var=tk.colorchooser.askcolor()
+    # print(color_var)#returns tuble of colour details
+    text_editor.configure(fg=color_var[1])
+font_btn.configure(command=change_font_color)
+
+#Align functionality
+def align_left():
+    text_content=text_editor.get(1.0,'end')
+    text_editor.tag_config('left',justify=tk.LEFT)
+    text_editor.delete(1.0,tk.END)
+    text_editor.insert(tk.INSERT,text_content,'left')
+left_bttn.configure(command=align_left)
+
+def align_right():
+    text_content=text_editor.get(1.0,'end')
+    text_editor.tag_config('right',justify=tk.RIGHT)
+    text_editor.delete(1.0,tk.END)
+    text_editor.insert(tk.INSERT,text_content,'right')
+right_bttn.configure(command=align_right)
+
+def align_centre():
+    text_content=text_editor.get(1.0,'end')
+    text_editor.tag_config('centre',justify=tk.CENTER)
+    text_editor.delete(1.0,tk.END)
+    text_editor.insert(tk.INSERT,text_content,'centre')
+centre_bttn.configure(command=align_centre)
+
+
+text_editor.configure(font=('Arial',12))
+
+
+# print(tk.font.Font(font=text_editor['font']).actual())
 #.......................................End of Text Editor.....................
 
 #......................................Main Status BAr.........................
 status_bar=tk.Label(application,text='Status Bar')
 status_bar.pack(side=tk.BOTTOM)
+
+
+def changed(event=None):
+    if text_editor.edit_modified():
+        words=len(text_editor.get(1.0,'end-1c').split())
+        ch=len(text_editor.get(1.0,'end-1c'))
+        status_bar.config(text=f"Characters : {ch}          Words: {words}")
+    text_editor.edit_modified(False)
+text_editor.bind("<<Modified>>",changed)
 
 #.......................................End of Status Bar.......................
 
